af692005bdb7c7bdc4f5f4c1859ad25ea773c1da
SSwiniarski <86081858+SSwiniarski@users.noreply.github.com>
2021-08-31 07:23:57 -0400

    Create inner_join_example.py

---
+++ understanding-different-types-of-joins-using-pandas/inner_join_example.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df1 = pd.DataFrame({'key': ['A', 'B', 'C', 'D'],
+                	'value': [1, 2, 3, 4]})
+
+df2 = pd.DataFrame({'key': ['C', 'D', 'E', 'F'],
+                	'value': [9, 8, 7, 6]})
+
+# The "suffixes" parameter is a tuple that specifies how to distinguish 
+# columns that have the same name in both dataframes.
+# The default value is ('_x','_y').
+
+df3 = pd.merge(df1,df2,on='key',how='inner', suffixes=('_left','_right'))
+
+print('Left Dataframe:\n')
+print(df1)
+print('\nRight Dataframe:\n')
+print(df2)
+print('\nInner Join on "key":\n')
+print(df3)
