1fd730a470873e22bce17b74a37c624b852b9950
SSwiniarski <86081858+SSwiniarski@users.noreply.github.com>
2021-09-08 09:15:06 -0400

    Update right_join_example.py

--- understanding-different-types-of-joins-using-pandas/right_join_example.py
+++ understanding-different-types-of-joins-using-pandas/right_join_example.py
@@ -6,16 +6,16 @@ df1 = pd.DataFrame({'this_key': ['A', 'B', 'C', 'D'],
 df2 = pd.DataFrame({'that_key': ['C', 'D', 'E', 'F'],
                 	'value': [9, 8, 7, 6]})
 
-# .merge() can be used as a method of a dataframe object.
-# When used this way, the dataframe using the method is the "left" dataframe.
+# .merge() can be used as a method of a DataFrame object.
+# When used this way, the DataFrame using the method is the "left" DataFrame.
 
 # Without using "suffixes", the default ones of "_x" and "_y" are used.
 
 df3 = df1.merge(df2,left_on='this_key',right_on='that_key',how='right')
 
-print('Left Dataframe:\n')
+print('Left DataFrame:\n')
 print(df1)
-print('\nRight Dataframe:\n')
+print('\nRight DataFrame:\n')
 print(df2)
 print('\nRight Join:\n')
 print(df3)
