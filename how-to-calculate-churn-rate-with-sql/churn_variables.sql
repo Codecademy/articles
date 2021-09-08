1bc88543854c4709a41cdcc5c4787b5324cd745a
SSwiniarski <86081858+SSwiniarski@users.noreply.github.com>
2021-09-08 14:12:19 -0400

    Create churn_variables.sql

---
+++ how-to-calculate-churn-rate-with-sql/churn_variables.sql
@@ -0,0 +1,38 @@
+DECLARE @period_start DATE
+DECLARE @period_end DATE
+
+SET @period_start = CAST('2020-01-01' AS DATE)
+SET @period_end = CAST('2020-01-31' AS DATE)
+
+WITH start_subscribers (subscriber_id)
+AS
+(SELECT 
+  subscribers.subscriber_id
+FROM
+  subscribers 
+WHERE
+  subscribers.start_date < @period_start AND
+  COALESCE(subscribers.end_date, @period_start) >= @period_start
+),
+
+churn_subscribers (subscriber_id)
+AS
+(SELECT
+  subscribers.subscriber_id
+FROM
+  subscribers 
+WHERE
+  subscribers.start_date < @period_start AND
+  subscribers.end_date >= @period_start AND
+  subscribers.end_date <= @period_end
+)
+
+SELECT
+  COUNT(start_subscribers.subscriber_id) AS inital_active_subscribers,
+  COUNT(churn_subscribers.subscriber_id) AS churn_subscribers,
+  (CAST(COUNT(churn_subscribers.subscriber_id) AS FLOAT) /
+  CAST(COUNT(start_subscribers.subscriber_id) AS FLOAT)) * 100.0 AS churn_rate
+FROM
+  start_subscribers 
+    LEFT JOIN churn_subscribers 
+    ON start_subscribers.subscriber_id = churn_subscribers.subscriber_id
