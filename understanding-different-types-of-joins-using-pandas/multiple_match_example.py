0db1a0e9a44c519b41aaa5c9b37ed97852565b2d
SSwiniarski <86081858+SSwiniarski@users.noreply.github.com>
2021-09-08 09:13:36 -0400

    Update multiple_match_example.py

--- understanding-different-types-of-joins-using-pandas/multiple_match_example.py
+++ understanding-different-types-of-joins-using-pandas/multiple_match_example.py
@@ -8,9 +8,9 @@ df2 = pd.DataFrame({'that_key': ['C', 'D', 'D', 'F'],
 
 df3 = df1.merge(df2,left_on='this_key',right_on='that_key',how='outer')
 
-print('Left Dataframe:\n')
+print('Left DataFrame:\n')
 print(df1)
-print('\nRight Dataframe:\n')
+print('\nRight DataFrame:\n')
 print(df2)
 print('\nOuter Join:\n')
 print(df3)
