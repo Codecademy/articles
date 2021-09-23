d94fcce47bb2a33f7fd619b1d22a6e7a11473632
Dusch4593 <brandondusch@gmail.com>
2021-09-23 15:29:01 -0400

    build: added source code for chat app tutorial.

---
+++ build-a-chat-app-with-socket-io/chat-app-src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const chatApp = express();
+const server = require('http').createServer(chatApp);
+const port = 3000;
+const io = require('socket.io')(server)
+const path =  require('path');
+
+chatApp.use(express.static(path.join(__dirname + '/public')));
+
+server.listen(port, () => {
+  console.log('Server running on port: ' + port);
+});
+
+io.on('connection', socket => {
+  console.log('Some clinet is connected.\n' + socket);
+
+  socket.on('chat', msg => {
+    console.log('From client', msg);
+  });
+});
+
