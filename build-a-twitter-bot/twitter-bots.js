de9b26b58bc5bede5a970bba2d445f3858713665
Dusch4593 <brandondusch@gmail.com>
2022-02-03 08:30:23 -0500

    style: reformatted with prettier

--- build-a-twitter-bot/twitter-bots.js
+++ build-a-twitter-bot/twitter-bots.js
@@ -1,36 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const Twit = require('twit')
-const twitAPIConfig = require('./config.js')
+const Twit = require("twit");
+const twitAPIConfig = require("./config.js");
 
-console.log("Starting twitter bot...")
+console.log("Starting twitter bot...");
 
-const twit = new Twit(twitAPIConfig) 
+const twit = new Twit(twitAPIConfig);
 
 const sendTweet = (text) => {
-  twit.post('statuses/update', { status: text }, (err, data, response) => {
-    if(err) throw err.message
-    console.log(data)
-  })
-}
+  twit.post("statuses/update", { status: text }, (err, data, response) => {
+    if (err) throw err.message;
+    console.log(data);
+  });
+};
 
-sendTweet("Hello, Twitterverse!")
+sendTweet("Hello, Twitterverse!");
 
-// const likeTweet = tweetIdString => {
-//   twit.post('favorites/create', { id: tweetIdString }, (err, data, response) => {
-//     if(err) throw err 
-//     console.log(data)
-//   })
-// }
+// const likeTweet = (tweetIdString) => {
+//   twit.post(
+//     "favorites/create",
+//     { id: tweetIdString },
+//     (err, data, response) => {
+//       if (err) throw err;
+//       console.log(data);
+//     }
+//   );
+// };
 
-// likeTweet("1423734592691900422")
+// likeTweet("1423734592691900422");
 
 // const follow = (twitterHandle, userID = null) => {
-//   twit.post("friendships/create", { screen_name: twitterHandle, id: userID }, (err, data, response) => {
-//     if(err) throw err
+//   twit.post(
+//     "friendships/create",
+//     { screen_name: twitterHandle, id: userID },
+//     (err, data, response) => {
+//       if (err) throw err;
 
-//     console.log(data)
-//   })
-// }
+//       console.log(data);
+//     }
+//   );
+// };
 
-// follow("codecademy")
+// follow("codecademy");
