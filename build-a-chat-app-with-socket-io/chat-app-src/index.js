befa1633699bf9deb70d607020a5cab77252008f
Dusch4593 <brandondusch@gmail.com>
2022-02-07 17:04:29 -0500

    stlye: ran prettier formatting

--- build-a-chat-app-with-socket-io/chat-app-src/index.js
+++ build-a-chat-app-with-socket-io/chat-app-src/index.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const chatApp = express();
-const server = require('http').createServer(chatApp);
+const server = require("http").createServer(chatApp);
 const port = 3000;
-const io = require('socket.io')(server)
-const path =  require('path');
+const io = require("socket.io")(server);
+const path = require("path");
 
-chatApp.use(express.static(path.join(__dirname + '/public')));
+chatApp.use(express.static(path.join(__dirname + "/public")));
 
 server.listen(port, () => {
-  console.log('Server running on port: ' + port);
+  console.log("Server running on port: " + port);
 });
 
-io.on('connection', socket => {
-  console.log('Some clinet is connected.\n' + socket);
+io.on("connection", (socket) => {
+  console.log("Some clinet is connected.\n" + socket);
 
-  socket.on('chat', msg => {
-    console.log('From client', msg);
+  socket.on("chat", (msg) => {
+    console.log("From client", msg);
   });
 });
-
